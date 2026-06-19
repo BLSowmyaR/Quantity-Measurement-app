10c2aa2f9f49b47aafa2c8f4aef2e9236cf7b2d6
BLSowmyaR <sowmya.ravi@bridgelabz.com>
2026-06-19 11:01:06 +0530

    Implement UC1: Feet measurement equality and its JUnit tests

---
+++ com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,62 @@
+package com.apps.quantitymeasurement;
+
+/**
+ * QuantityMeasurementApp - UC1: Feet measurement equality
+ *
+ * This class is responsible for checking the equality of two numerical values
+ * measured in feet in the Quantity Measurement Application.
+ */
+public class QuantityMeasurementApp {
+    // Inner class to represent Feet measurement
+    public static class Feet {
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        /**
+         * Override equals() method to compare two Feet objects based on their value
+         * 
+         * <p>Important Checks:</p>
+         * 1. Reference Check: If both references point to the same object, return true
+         * 2. Null Check: If the compared object is null, return false
+         * 3. Type Check: If the compared object is not of type Feet, return false
+         * 4. Value Comparison: Use Double.compare() to compare the double values for equality
+         * 
+         * @param obj The object to compare with
+         * @return true if both Feet objects have the same value, false otherwise
+         */
+        @Override
+        public boolean equals(Object obj) {
+            // 1. Reference Check: If both references point to the same object, return true
+            if (this == obj) {
+                return true;
+            }
+            // 2. Null Check: If the compared object is null, return false
+            if (obj == null) {
+                return false;
+            }
+            // 3. Type Check: If the compared object is not of type Feet, return false
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            // 4. Value Comparison: Use Double.compare() to compare the double values for equality
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // Main method to demonstrate Feet equality check
+    public static void main(String[] args) {
+        Feet feet1 = new Feet(1.0);
+        Feet feet2 = new Feet(1.0);
+        Feet feet3 = new Feet(2.0);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + feet1.equals(feet2) + ")");
+
+        System.out.println("Input: 1.0 ft and 2.0 ft");
+        System.out.println("Output: Equal (" + feet1.equals(feet3) + ")");
+    }
+}
