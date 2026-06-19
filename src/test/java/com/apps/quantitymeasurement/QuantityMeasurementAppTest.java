59f06f18614a45f62780212943f6701fcae9ddda
BLSowmyaR <sowmya.ravi@bridgelabz.com>
2026-06-19 11:33:16 +0530

    Implement UC2: Inches measurement equality and its JUnit tests

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
+
+    // --- Feet Equality Tests ---
 
     @Test
     public void testFeetEquality_SameValue() {
@@ -38,5 +41,40 @@ public class QuantityMeasurementAppTest {
     public void testFeetEquality_SameReference() {
         Feet feet = new Feet(1.0);
         assertTrue(feet.equals(feet), "Feet object compared with itself should return true");
+    }
+
+    // --- Inches Equality Tests ---
+
+    @Test
+    public void testInchesEquality_SameValue() {
+        Inches inches1 = new Inches(1.0);
+        Inches inches2 = new Inches(1.0);
+        assertTrue(inches1.equals(inches2), "Two Inches objects with the same value should be equal");
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+        Inches inches1 = new Inches(1.0);
+        Inches inches2 = new Inches(2.0);
+        assertFalse(inches1.equals(inches2), "Two Inches objects with different values should not be equal");
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+        Inches inches = new Inches(1.0);
+        assertFalse(inches.equals(null), "Inches object compared with null should return false");
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass() {
+        Inches inches = new Inches(1.0);
+        Object obj = new Object();
+        assertFalse(inches.equals(obj), "Inches object compared with an object of different class should return false");
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+        Inches inches = new Inches(1.0);
+        assertTrue(inches.equals(inches), "Inches object compared with itself should return true");
     }
 }
